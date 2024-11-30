5f797e7ab48d0d5ce97683662a5f71d861ff0b62
k1m190r <k1m190r@outlook.com>
2024-11-30 01:21:12 +0100

    91 working

--- 91/main.cpp
+++ 91/main.cpp
@@ -1,10 +1,10 @@
 #include <print>
 #include <ranges>
 #include <string>
+#include <vector>
 
-// #include "../../AOC2023/demangle.h"
+using std::string, std::print,std::vector;
 
-using std::string, std::print;
 namespace R = std::ranges;
 namespace V = std::ranges::views;
 using namespace std::string_literals;
@@ -21,8 +21,7 @@ int main() {
   {
     string s = "124 2342"s;
     auto fn = [](auto v) { return 100 * std::stoi(string(v.begin(), v.end())); };
-    auto s2 = s | V::split(' ') | V::transform(fn) | V::reverse;
-    // | V::reverse | R::to<string>;
+    auto s2 = s | V::split(' ') | V::transform(fn) | R::to<vector>() | V::reverse;
     print("\n{}\n", s2);
   }
 
