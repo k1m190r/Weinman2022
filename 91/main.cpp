d4432d9bf28de7222767db64bca76b80decc33fa
k1m190r <k1m190r@outlook.com>
2024-11-30 02:49:56 +0100

    91

--- 91/main.cpp
+++ 91/main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <vector>
 
-using std::string, std::print,std::vector;
+using std::string, std::println, std::vector;
 
 namespace R = std::ranges;
 namespace V = std::ranges::views;
@@ -22,11 +22,11 @@ int main() {
     string s = "124 2342"s;
     auto fn = [](auto v) { return 100 * std::stoi(string(v.begin(), v.end())); };
     auto s2 = s | V::split(' ') | V::transform(fn) | R::to<vector>() | V::reverse;
-    print("\n{}\n", s2);
+    println("\n{}", s2);
   }
 
   {
     auto d = 1923'234'3;
-    print("{}", d);
+    println("{}", d);
   }
 }
